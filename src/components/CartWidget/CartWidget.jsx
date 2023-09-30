2d4fb482405ec353b88ef1a209bf9a682832b355
miguegarav <miguelgarav@gmail.com>
2023-09-30 12:09:19 -0300

    proyectoReact

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,13 @@
+import cart from "./assests/Cart.png"
+
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src={cart} alt="cart-widget " style={{ width: "50px" }} />
+                              0
+        </div>
+    )
+}
+
+export default CartWidget
