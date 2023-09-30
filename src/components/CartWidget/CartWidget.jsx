b84e2fff842dae258a379db3e75e77682f0a793d
miguegarav <miguelgarav@gmail.com>
2023-09-30 13:22:53 -0300

    React+Garavaglia

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -4,8 +4,8 @@ import cart from "./assests/Cart.png"
 const CartWidget = () => {
     return (
         <div>
-            <img src={cart} alt="cart-widget " style={{ width: "50px" }} />
-                              0
+            <img src={cart} alt="cart-widget " style={{ width: "10px" }} />
+            0
         </div>
     )
 }
